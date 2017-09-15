7d3cc38998433ac8e13a565ddb702ff35e4a9fe3
Przemyslaw Zygmunt <p.zygmunt@acsoftware.pl>
2017-09-15 15:56:34 +0200

    Unuser import

--- app/src/main/java/org/supla/android/lib/SuplaClient.java
+++ app/src/main/java/org/supla/android/lib/SuplaClient.java
@@ -26,7 +26,6 @@ import android.os.Build;
 import android.os.Handler;
 
 import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
