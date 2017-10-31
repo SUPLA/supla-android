58466981e78b70e8a1b9eecfb66e2074ab14c764
Przemyslaw Zygmunt <p.zygmunt@acsoftware.pl>
2017-10-31 11:25:54 +0100

    License update

--- app/src/main/java/org/supla/android/listview/LineView.java
+++ app/src/main/java/org/supla/android/listview/LineView.java
@@ -7,6 +7,8 @@ import android.widget.RelativeLayout;
 import org.supla.android.R;
 
 /*
+ Copyright (C) AC SOFTWARE SP. Z O.O.
+
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
  as published by the Free Software Foundation; either version 2
@@ -20,8 +22,6 @@ import org.supla.android.R;
  You should have received a copy of the GNU General Public License
  along with this program; if not, write to the Free Software
  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-
- Author: Przemyslaw Zygmunt p.zygmunt@acsoftware.pl [AC SOFTWARE]
  */
 
 public class LineView extends View {
