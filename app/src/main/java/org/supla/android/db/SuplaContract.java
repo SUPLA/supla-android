3a6799452aba9e15a11b26c817832299757ddcea
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2019-02-21 16:34:10 +0100

    UserIconsEntry

--- app/src/main/java/org/supla/android/db/SuplaContract.java
+++ app/src/main/java/org/supla/android/db/SuplaContract.java
@@ -198,4 +198,15 @@ public class SuplaContract {
         public static final String COLUMN_NAME_PHASE3_FAE = "phase3_fae";
 
     }
+
+    public static abstract class UserIconsEntry implements BaseColumns {
+
+        public static final String TABLE_NAME = "user_icons";
+
+        public static final String _ID = "_id"; // Primary Key
+        public static final String COLUMN_NAME_REMOTEID = "remoteid";
+        public static final String COLUMN_NAME_IMAGE1 = "image1";
+        public static final String COLUMN_NAME_IMAGE2 = "image2";
+        public static final String COLUMN_NAME_IMAGE3 = "image3";
+    }
 }
