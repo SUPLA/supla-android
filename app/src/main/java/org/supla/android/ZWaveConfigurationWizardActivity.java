d3548cd2a925acb8e8a8ab0fdc9b095add0cdedc
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2020-01-24 12:48:51 +0100

    registration_flasg field added

--- app/src/main/java/org/supla/android/ZWaveConfigurationWizardActivity.java
+++ app/src/main/java/org/supla/android/ZWaveConfigurationWizardActivity.java
@@ -1,11 +1,28 @@
 package org.supla.android;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ZWaveConfigurationWizardActivity extends NavigationActivity {
+
+    private Button btnTest;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zwave);
+
+        btnTest = findViewById(R.id.btnTest);
+        btnTest.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        super.onClick(v);
+
+        if (v == btnTest) {
+            SuplaApp.getApp().getSuplaClient().getChannelState(298);
+        }
     }
 }
