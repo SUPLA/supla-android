725669bed53da836637a5906ed5c847b4dab6f3f
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2020-06-07 17:48:06 +0200

    SuplaColorBrightnessPickerTest class skeleton

---
+++ app/src/test/java/org/supla/android/SuplaColorBrightnessPickerTest.java
@@ -0,0 +1,19 @@
+package org.supla.android;
+
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class SuplaColorBrightnessPickerTest extends TestCase {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
