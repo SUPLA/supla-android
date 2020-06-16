10a1a9579d7bd37c09bcc9bdc0974937e8d133a9
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2020-06-16 23:54:35 +0200

    MIN_UPDATE_DELAY set to 3 sec

--- app/src/main/java/org/supla/android/ChannelDetailRGBW.java
+++ app/src/main/java/org/supla/android/ChannelDetailRGBW.java
@@ -53,7 +53,7 @@ public class ChannelDetailRGBW extends DetailLayout implements View.OnClickListe
         SuplaColorListPicker.OnColorListTouchListener {
 
     final static private long MIN_REMOTE_UPDATE_PERIOD = 250;
-    final static private long MIN_UPDATE_DELAY = 3500;
+    final static private long MIN_UPDATE_DELAY = 3000;
     private SuplaColorBrightnessPicker cbPicker;
     private SuplaColorListPicker clPicker;
     private Button tabRGB;
