331a24bc03641bb5d23284ab4381994596f1178a
pki <pki-acs@ex.com.pl>
2022-04-18 10:57:55 +0200

    UI for profile selection

--- app/src/main/java/org/supla/android/cfg/ProfileItemViewModel.kt
+++ app/src/main/java/org/supla/android/cfg/ProfileItemViewModel.kt
@@ -3,7 +3,7 @@ package org.supla.android.cfg
 import androidx.databinding.BaseObservable
 import androidx.databinding.Bindable
 
-class ProfileItemViewModel(val t: String, val isActive: Boolean): BaseObservable() {
+open class ProfileItemViewModel(val t: String, val isActive: Boolean): BaseObservable() {
     @Bindable var title: String? = t
     @Bindable var active: Boolean? = isActive
 }
