c83b1a2f4256dda912c8e99e256022b358c66f3a
pki <pki-acs@ex.com.pl>
2022-05-09 20:16:53 +0200

    more readable syntax used

--- app/src/main/java/org/supla/android/Preferences.java
+++ app/src/main/java/org/supla/android/Preferences.java
@@ -105,8 +105,11 @@ public class Preferences {
             if (_prefs.getBoolean(pref_key + "_encrypted", false)) {
                 result = Encryption.decryptDataWithNullOnException(result, getDeviceID(), true);
             }
-            if(result != null)
+            
+            if(result != null) {
                 encryptAndSave(pref_key, result);
+            }
+            
         } else {
             result = Encryption.decryptDataWithNullOnException(result, getDeviceID());
         }
