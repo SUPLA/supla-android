9ec2783bd972f3f3d21ee7cc20667325c1479c4b
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2022-12-14 13:17:17 +0100

    Version code raised to 164

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = Versions.MinSdk
         targetSdk = Versions.TargetSdk
         multiDexEnabled = true
-        versionCode = 163
+        versionCode = 164
         versionName = "22.12"
 
         ndk {
