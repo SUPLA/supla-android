696928650115f2e09b18decf51f2a186737c3be7
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2023-07-18 17:50:25 +0200

    Version changed to 23.07.01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
     minSdk = Versions.MinSdk
     targetSdk = Versions.TargetSdk
     multiDexEnabled = true
-    versionCode = 187
-    versionName = "23.07"
+    versionCode = 188
+    versionName = "23.07.01"
 
     ndk {
       moduleName = "suplaclient"
