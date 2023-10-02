b0c2216e67778e6ca75e8ba0bbcf40e4b3998d57
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2023-10-02 22:58:36 +0200

    Revert "Version changed to 23.10.01"
    This reverts commit 481de5e04c0ce03a6bfd3d3722af3ca0cb9882d5.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
     minSdk = Versions.MinSdk
     targetSdk = Versions.TargetSdk
     multiDexEnabled = true
-    versionCode = 198
-    versionName = "23.10.01"
+    versionCode = 197
+    versionName = "23.10"
 
     ndk {
       moduleName = "suplaclient"
