f1741f0907d3dc584235d5dd6a61779c775ca9ec
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2024-09-20 16:30:56 +0200

    Change versionCode to 244

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
     minSdk = Versions.MinSdk
     targetSdk = Versions.TargetSdk
     multiDexEnabled = true
-    versionCode = 243
+    versionCode = 244
     versionName = "24.09.01"
 
     ndk {
