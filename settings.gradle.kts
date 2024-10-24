b4ada8f280c1cd706f20281a6b1b090327c8653e
Michal Polanski <michal@polanscy.com.pl>
2024-10-24 10:05:33 +0200

    Update dependencies versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,20 @@
+
+pluginManagement {
+  repositories {
+    google()
+    mavenCentral()
+    gradlePluginPortal()
+  }
+}
+
+dependencyResolutionManagement {
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositories {
+    google()
+    mavenCentral()
+    maven("https://jitpack.io")
+  }
+}
+
 include(":app")
 include(":shared-core")
