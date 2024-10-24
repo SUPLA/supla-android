77e776bf35d49244cee0c9b0c264811eece770dd
Michal Polanski <michal@polanscy.com.pl>
2024-10-24 11:42:06 +0200

    Correct pattern after jsoup migration

--- app/src/main/java/org/supla/android/core/networking/esp/EspHtmlParser.kt
+++ app/src/main/java/org/supla/android/core/networking/esp/EspHtmlParser.kt
@@ -25,7 +25,7 @@ import java.util.regex.Pattern
 import javax.inject.Inject
 import javax.inject.Singleton
 
-private const val STATE_PATTERN = "^LAST\\ STATE:\\ (.*)\\<br\\>Firmware:\\ (.*)\\<br\\>GUID:\\ (.*)\\<br\\>MAC:\\ (.*)$"
+private const val STATE_PATTERN = "LAST\\ STATE:\\ (.*)\\<br\\>[\\n ]*Firmware:\\ (.*)\\<br\\>[\\n ]*GUID:\\ (.*)\\<br\\>[\\n ]*MAC:\\ (.*)"
 
 private const val INPUT_NAME_NO_CHANNELS = "no_visible_channels"
 private const val INPUT_VALUE_NO_CHANNELS = "1"
@@ -73,8 +73,8 @@ class EspHtmlParser @Inject constructor() {
 
     result.needsCloudConfig = fieldMap[INPUT_NAME_NO_CHANNELS]?.let { it == INPUT_VALUE_NO_CHANNELS } ?: false
 
-    doc.getElementsByTag("h1")?.also {
-      it.next()?.also { elements ->
+    doc.getElementsByTag("h1").also {
+      it.next().also { elements ->
         for (element in elements) {
           if (element.html().contains("LAST STATE").not()) {
             continue
@@ -83,7 +83,7 @@ class EspHtmlParser @Inject constructor() {
           val pattern = Pattern.compile(STATE_PATTERN)
           val matcher = pattern.matcher(element.html())
           if (matcher.find() && matcher.groupCount() == 4) {
-            result.deviceName = it.html()
+            result.deviceName = it.text()
             result.deviceLastState = matcher.group(1)
             result.deviceFirmwareVersion = matcher.group(2)
             result.deviceGUID = matcher.group(3)
