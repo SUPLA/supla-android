a7087fa9a8b5d9e8979074afe9cb1047090e4a4e
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2025-07-08 21:22:45 +0200

    Change versionCode to 293

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
     minSdk = libs.versions.minSdk.get().toInt()
     targetSdk = libs.versions.targetSdk.get().toInt()
     multiDexEnabled = true
-    versionCode = 292
+    versionCode = 293
     versionName = "25.07"
 
     ndk {
