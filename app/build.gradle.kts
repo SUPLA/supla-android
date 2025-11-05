2b190d45eb92f10681b2f48c5cf30c71e459557f
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2025-11-05 19:31:01 +0100

    Change version code to 307

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
     minSdk = libs.versions.minSdk.get().toInt()
     targetSdk = libs.versions.targetSdk.get().toInt()
     multiDexEnabled = true
-    versionCode = 306
+    versionCode = 307
     versionName = "25.11"
 
     ndk {
