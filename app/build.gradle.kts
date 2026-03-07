a9ba0b44121e32a8dcd5c95abfe50658887b19a8
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2026-03-07 20:27:18 +0100

    Change versionCode to 317

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
     minSdk = libs.versions.minSdk.get().toInt()
     targetSdk = libs.versions.targetSdk.get().toInt()
     multiDexEnabled = true
-    versionCode = 313
-    versionName = "26.01"
+    versionCode = 317
+    versionName = "26.03"
 
     ndk {
       moduleName = "suplaclient"
