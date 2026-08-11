95eac040b787f3abcaeaa49e6e5ae0cba4b0296b
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2026-08-11 23:07:07 +0200

    Change versionCode to 332

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
     applicationId = "org.supla.android"
     minSdk = libs.versions.minSdk.get().toInt()
     targetSdk = libs.versions.targetSdk.get().toInt()
-    versionCode = 331
+    versionCode = 332
     versionName = "26.08"
 
     ndk {
